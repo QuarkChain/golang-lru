95799037c92b9637523c3456718004ebebb093ad
Qi Zhou <qzhou64@gmail.com>
2021-12-13 12:58:49 -0800

    add lru with accounting

---
+++ simplelru/lru_accounting.go
@@ -0,0 +1,188 @@
+package simplelru
+
+import (
+	"container/list"
+	"errors"
+)
+
+// EvictCallback is used to get a callback when a cache entry is evicted
+
+type AccountCallback func(key interface{}, value interface{}) int
+
+// LRU implements a non-thread safe fixed size LRU cache
+type LRUWithAccounting struct {
+	limit     int
+	size      int
+	evictList *list.List
+	items     map[interface{}]*list.Element
+	onEvict   EvictCallback
+	onAccount AccountCallback
+}
+
+// NewLRU constructs an LRU of the given size
+func NewLRUWithAccounting(limit int, onAccount AccountCallback, onEvict EvictCallback) (*LRUWithAccounting, error) {
+	if limit <= 0 {
+		return nil, errors.New("must provide a positive size")
+	}
+	c := &LRUWithAccounting{
+		limit:     limit,
+		evictList: list.New(),
+		items:     make(map[interface{}]*list.Element),
+		onEvict:   onEvict,
+		onAccount: onAccount,
+	}
+	return c, nil
+}
+
+// Purge is used to completely clear the cache.
+func (c *LRUWithAccounting) Purge() {
+	for k, v := range c.items {
+		if c.onEvict != nil {
+			c.onEvict(k, v.Value.(*entry).value)
+		}
+		delete(c.items, k)
+	}
+	c.evictList.Init()
+	c.size = 0
+}
+
+// Add adds a value to the cache.  Returns true if an eviction occurred.
+func (c *LRUWithAccounting) Add(key, value interface{}) (evicted bool) {
+	// Check for existing item
+	if ent, ok := c.items[key]; ok {
+		c.evictList.MoveToFront(ent)
+		ent.Value.(*entry).value = value
+		return false
+	}
+
+	// Add new item
+	ent := &entry{key, value}
+	entry := c.evictList.PushFront(ent)
+	c.items[key] = entry
+	c.size += c.onAccount(key, value)
+
+	return c.evictIfNeeded()
+}
+
+func (c *LRUWithAccounting) evictIfNeeded() (evicted bool) {
+	evict := c.size > c.limit
+
+	for c.size > c.limit {
+		c.removeOldest()
+	}
+
+	return evict
+}
+
+// Get looks up a key's value from the cache.
+func (c *LRUWithAccounting) Get(key interface{}) (value interface{}, ok bool) {
+	if ent, ok := c.items[key]; ok {
+		c.evictList.MoveToFront(ent)
+		if ent.Value.(*entry) == nil {
+			return nil, false
+		}
+		return ent.Value.(*entry).value, true
+	}
+	return
+}
+
+// Contains checks if a key is in the cache, without updating the recent-ness
+// or deleting it for being stale.
+func (c *LRUWithAccounting) Contains(key interface{}) (ok bool) {
+	_, ok = c.items[key]
+	return ok
+}
+
+// Peek returns the key value (or undefined if not found) without updating
+// the "recently used"-ness of the key.
+func (c *LRUWithAccounting) Peek(key interface{}) (value interface{}, ok bool) {
+	var ent *list.Element
+	if ent, ok = c.items[key]; ok {
+		return ent.Value.(*entry).value, true
+	}
+	return nil, ok
+}
+
+// Remove removes the provided key from the cache, returning if the
+// key was contained.
+func (c *LRUWithAccounting) Remove(key interface{}) (present bool) {
+	if ent, ok := c.items[key]; ok {
+		c.removeElement(ent)
+		return true
+	}
+	return false
+}
+
+// RemoveOldest removes the oldest item from the cache.
+func (c *LRUWithAccounting) RemoveOldest() (key, value interface{}, ok bool) {
+	ent := c.evictList.Back()
+	if ent != nil {
+		c.removeElement(ent)
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
+	}
+	return nil, nil, false
+}
+
+// GetOldest returns the oldest entry
+func (c *LRUWithAccounting) GetOldest() (key, value interface{}, ok bool) {
+	ent := c.evictList.Back()
+	if ent != nil {
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
+	}
+	return nil, nil, false
+}
+
+// Keys returns a slice of the keys in the cache, from oldest to newest.
+func (c *LRUWithAccounting) Keys() []interface{} {
+	keys := make([]interface{}, len(c.items))
+	i := 0
+	for ent := c.evictList.Back(); ent != nil; ent = ent.Prev() {
+		keys[i] = ent.Value.(*entry).key
+		i++
+	}
+	return keys
+}
+
+// Len returns the number of items in the cache.
+func (c *LRUWithAccounting) Len() int {
+	return c.evictList.Len()
+}
+
+// Resize changes the cache size.
+func (c *LRUWithAccounting) Resize(size int) (evicted int) {
+	diff := c.Len() - size
+	if diff < 0 {
+		diff = 0
+	}
+	for i := 0; i < diff; i++ {
+		c.removeOldest()
+	}
+	c.limit = size
+	return diff
+}
+
+// removeOldest removes the oldest item from the cache.
+func (c *LRUWithAccounting) removeOldest() {
+	ent := c.evictList.Back()
+	if ent != nil {
+		c.removeElement(ent)
+	}
+}
+
+// AccountingSize returns the size of the cache measured by accounting func.
+func (c *LRUWithAccounting) AccountingSize() int {
+	return c.size
+}
+
+// removeElement is used to remove a given list element from the cache
+func (c *LRUWithAccounting) removeElement(e *list.Element) {
+	c.evictList.Remove(e)
+	kv := e.Value.(*entry)
+	delete(c.items, kv.key)
+	if c.onEvict != nil {
+		c.onEvict(kv.key, kv.value)
+	}
+	c.size -= c.onAccount(kv.key, kv.value)
+}
---
+++ simplelru/lru_accounting_test.go
@@ -0,0 +1,86 @@
+package simplelru
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLRUWithAccounting(t *testing.T) {
+	evictCounter := 0
+	onEvicted := func(k interface{}, v interface{}) {
+		if k != string(v.([]byte)) {
+			t.Fatalf("Evict values not equal (%v!=%v)", k, v)
+		}
+		evictCounter++
+	}
+	onAccount := func(k interface{}, v interface{}) int {
+		return len(k.(string)) + len(v.([]byte))
+	}
+	l, err := NewLRUWithAccounting(10, onAccount, onEvicted)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		l.Add(fmt.Sprint(i), []byte(fmt.Sprint(i)))
+	}
+	if l.AccountingSize() != 10 {
+		t.Fatalf("bad size: %v", l.AccountingSize())
+	}
+
+	if evictCounter != 5 {
+		t.Fatalf("bad evict count: %v", evictCounter)
+	}
+
+	for i, k := range l.Keys() {
+		if v, ok := l.Get(k); !ok || string(v.([]byte)) != k || string(v.([]byte)) != fmt.Sprint(i+5) {
+			t.Fatalf("bad key: %v", k)
+		}
+	}
+	for i := 0; i < 5; i++ {
+		_, ok := l.Get(fmt.Sprint(i))
+		if ok {
+			t.Fatalf("should be evicted")
+		}
+	}
+	for i := 5; i < 10; i++ {
+		_, ok := l.Get(fmt.Sprint(i))
+		if !ok {
+			t.Fatalf("should not be evicted")
+		}
+	}
+	for i := 5; i < 7; i++ {
+		k := fmt.Sprint(i)
+		ok := l.Remove(k)
+		if !ok {
+			t.Fatalf("should be contained")
+		}
+		ok = l.Remove(k)
+		if ok {
+			t.Fatalf("should not be contained")
+		}
+		_, ok = l.Get(k)
+		if ok {
+			t.Fatalf("should be deleted")
+		}
+	}
+
+	l.Get(fmt.Sprint(7)) // expect 5 to be last key in l.Keys()
+
+	for i, k := range l.Keys() {
+		if (i < 2 && k != fmt.Sprint(i+8)) || (i == 2 && k != "7") {
+			t.Fatalf("out of order key: %v", k)
+		}
+	}
+
+	l.Purge()
+	if l.Len() != 0 {
+		t.Fatalf("bad len: %v", l.Len())
+	}
+	if l.AccountingSize() != 0 {
+		t.Fatalf("bad size: %v", l.Len())
+	}
+	if _, ok := l.Get(8); ok {
+		t.Fatalf("should contain nothing")
+	}
+}
